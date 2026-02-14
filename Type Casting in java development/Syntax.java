d15f8b1ff3d6e6fb87fb13b984a1ea49fe5243fb
Dawood Sattar <dawoodsattar007@gmail.com>
2026-02-15 02:35:35 +0500

    Clarify comments on type casting examples
    I just added widening type casting also called Implicit and narrowing type casting also called Explicit type casting

--- Type Casting in java development/Syntax.java
+++ Type Casting in java development/Syntax.java
@@ -2,12 +2,12 @@ public class Syntax {
     public static void main(String[] args) {
         {/* TYPE CASTING */}
 
-        // 1. Widening Type Casting 
+        // 1. Widening Type Casting or Implicit
         int a = 10;
         double b = a;
         System.out.println(b);
 
-        // 2. Narrowing Type Casting 
+        // 2. Narrowing Type Casting or Explicit
         double c = 10.5;
         int d = (int) c;
         System.out.println(d);
